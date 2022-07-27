56f277d1bf17bdb3a5d0c90c5799e5e565be1fa4
Maidul Bappy <maidulislambappy@gmail.com>
2022-07-27 18:44:09 +0600

    Project test

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# maidulislambappy-uptime-monitoring-api-raw-node- # maidulislambappy-uptime-monitoring-api-raw-node-
+/\*
+
+- Title: Uptime Monitoring Api Raw Node
+- Description: Uptime Monitoring Application
+- Author: Maidul Bappy
+- Date: 27/07/22
+- ::::::::Feature::::::::
+- Start the API Server
+- Create, edit, delete user
+- token based authenticaion
+- logout mechanism
+- lset links & up/down
+- edit/delite links & rate limit
+- chech up down time
+
+  \*/
--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-/*
- * Title: Uptime Monitoring Api Raw Node
- * Description: Uptime Monitoring Application
- * Author: Maidul Bappy
- * Date: 27/07/22
- * ::::::::Feature::::::::
- * Start the API Server
- * Create, edit, delete user
- * token based authenticaion
- * logout mechanism
- * lset links & up/down
- * edit/delite links & rate limit
- * chech up down time
- */
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello World!');
+}).listen(8080);
