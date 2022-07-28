a16886c67a2abbd32f6a5748c1a9e0ec67185b7b
Maidul Bappy <maidulislambappy@gmail.com>
2022-07-28 23:43:06 +0600

    html created

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+/*
+- Title: Uptime Monitoring Api Raw Node
+- Description: Uptime Monitoring Application
+- Author: Maidul Bappy
+- Date: 27/07/22
+*/
+
+// Dependencies
 const http = require('http');
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World!');
-}).listen(8080);
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log('listening to port: 3000');
+    });
+};
+// handle request response
+
+app.handleReqRes = (req, res) => {
+    // response handle
+    res.end('Hello World');
+};
